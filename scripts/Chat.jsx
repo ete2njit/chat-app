c7b2cd6e1863c520f7801584d4d0a33f420b7501
EC2 Default User <ec2-user@ip-172-31-61-147.ec2.internal>
2020-10-09 20:27:48 +0000

    Add clientside functionality to change how many texts from the chat log are displayed

--- scripts/Chat.jsx
+++ scripts/Chat.jsx
@@ -3,7 +3,8 @@ import { Socket } from './Socket';
 
 
 export function Chat(props) {
-    const [displayCount, setDisplayCount] = React.useState(2);      // display count of 0 means show the whole chatlog
+    // display count of 0 means show the whole chatlog
+    const [displayCount, setDisplayCount] = React.useState(5);      
     let chat = [];
     let chatLength = props.chatlog.length;
     
@@ -15,13 +16,23 @@ export function Chat(props) {
         {
             chat[displayCount-i] = props.chatlog[chatLength-1-i];
         }
+        
     
-    
+    function handleChange(event) {
+        setDisplayCount(event.target.value);
+    }
     
     return (
         <ol>
             {chat.map((address, index) =>
                 <li key={index}>{address}</li>)}
+                
+            <input
+                type="text"
+                value={displayCount}
+                onChange={handleChange}
+            />
+        
         </ol>
     );
 }
