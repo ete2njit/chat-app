415c8f8caeccf7e17e48be17a41890ce5c9bb37e
EC2 Default User <ec2-user@ip-172-31-61-147.ec2.internal>
2020-10-13 01:48:31 +0000

    Change database url to be in env, rather than constructed in app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ load_dotenv(dotenv_path)
 sql_user = os.environ['SQL_USER']
 sql_pwd = os.environ['SQL_PASSWORD']
 
-database_uri = 'postgresql://{}:{}@localhost/postgres'.format(sql_user, sql_pwd)
+database_uri = os.environ['DATABASE_URL']
 
 app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
 
--- chatbot.py
+++ chatbot.py
@@ -1,3 +1,5 @@
+from random import choice
+
 class Chatbot:
     name = ""
     key = ""
@@ -10,18 +12,27 @@ class Chatbot:
         return "A long time ago, in a chat room far far away there lived a chatbot with functionality that can be found when typing !! help"
     
     def help(self):
-        return "Type '!! about' for info about me! Type '!! funtranslate <message>' and I will translate your message!"
+        return "Type '!! about' for info about me! Type '!! funtranslate <message>' and I will translate your message. '!! randcase <message>' and I'll return your message with random capitalization."
+        
+    def randcase(self, sentence):
+        return ''.join(choice((str.upper, str.lower))(c) for c in sentence)
     
     def process(self, argument):
-        command = argument.strip()[2:].strip()
-        if command == "about":
+        line = argument.strip()[2:].strip()
+        arg_list = line.split()
+        command = arg_list[0]
+        param = " ".join(arg_list[1:])
+        
+        if command.lower() == "about":
             return self.about()
-        if command == "help":
+        if command.lower() == "help":
             return self.help()
+        if command.lower() == "randcase":
+            return self.randcase(param)
         
         
         
-        return "'" + command + "' is not a recognized command. Type !! help for info about recognized commands"
+        return "'" + command.lower() + "' is not a recognized command. Type !! help for info about recognized commands"
         
     def isCommand(self, argument):
         if argument.strip()[0] == '!' and argument.strip()[1] == '!':
