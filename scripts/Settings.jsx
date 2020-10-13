7e6557781604afead1aa0573ccd0ba6c187291ba
EC2 Default User <ec2-user@ip-172-31-61-147.ec2.internal>
2020-10-13 14:55:22 +0000

    Add a window for settings and make it a mirror of the information window

--- scripts/Settings.jsx
+++ scripts/Settings.jsx
@@ -1,19 +1,18 @@
 import * as React from 'react';
-//import "../static/settingstyle.css";
+import "../static/settingstyle.css";
 
 
 export function Settings(props) {
     return (
-        <div>
-            <div className="displayCount">
-                <input
+        <div className="settings-window">
+            <div className="settings-content">
+                <span className="display-count">Chat length:  </span>
+                <input className="display-input"
                     type="text"
                     value={ props.displayCount }
                     onChange={ props.handleChange }
                 />
             </div>
-            
-            
         </div>
     )
 }
