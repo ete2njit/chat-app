6b124d596a3088de019e9517b5eaeda56fec819c
EC2 Default User <ec2-user@ip-172-31-61-147.ec2.internal>
2020-10-28 00:24:10 +0000

    Add tests that use mocking to fully cover chatbot, and partially cover app

---
+++ tests/mocked_unit_tests.py
@@ -0,0 +1,99 @@
+import unittest
+import unittest.mock as mock
+from dotenv import load_dotenv
+import os
+import datetime
+from os.path import join, dirname
+import sys
+import json
+from requests.models import Response
+
+sys.path.append(join(dirname(__file__), "../"))
+
+from chatbot import Chatbot
+from models import Message
+import app
+
+KEY_INPUT = "input"
+KEY_EXPECTED = "expected"
+
+
+class ChatAppTestCase(unittest.TestCase):
+    def setUp(self):
+        self.chatbot_funtranslate_test = [
+            {
+                KEY_INPUT: "Sentence to translate",
+                KEY_EXPECTED: "Sentence to translate",
+            },
+        ]
+        
+        self.chatbot_joke_test = [
+            {
+                KEY_INPUT: "",
+                KEY_EXPECTED: "this is a joke",
+            }
+        ]
+        self.chatbot_randcase_test = [
+            {
+                KEY_INPUT: "Sentence to randcase",
+                KEY_EXPECTED: "sentence to randcase",
+            }
+        ]
+        self.app_login_requested_test = [
+            {
+                KEY_INPUT: {'type': "GOOGLE", 'data':{'profileObj':{'name': "Bill", "googleId": "Bills Google Id", "imageUrl": "Bills Image Url"}}},
+                KEY_EXPECTED: {
+                    "username": "Bill",
+                    "userkey": "GOOGLEBills Google Id",
+                    "userimage": "Bills Image Url",
+                },
+            }
+            ]
+
+       
+    
+    def mocked_funtranslate_get(self, sentence):
+        response = mock.Mock(spec=Response)
+        response.json.return_value = { "contents": {"translated": sentence[57:]}}
+        return response
+    
+    def mocked_joke_get(self, url):
+        response = mock.Mock(spec=Response)
+        response.json.return_value = [{ "setup": "this is",  "punchline": "a joke"}]
+        return response
+    
+    def mocked_random_choice(self, values):
+        return values[1]
+        
+    def mocked_socketio_emit(self, channel, data, requestid):
+        return
+    
+    def test_chatbot(self):
+        bot = Chatbot()
+        
+        for test_case in self.chatbot_funtranslate_test:
+            with mock.patch('requests.get', self.mocked_funtranslate_get):
+                bot_response = bot.translate(test_case[KEY_INPUT])
+                
+                self.assertEqual(test_case[KEY_EXPECTED], bot_response)
+                
+        for test_case in self.chatbot_joke_test:
+            with mock.patch('requests.get', self.mocked_joke_get):
+                bot_response = bot.joke()
+                
+                self.assertEqual(test_case[KEY_EXPECTED], bot_response)
+        
+        for test_case in self.chatbot_randcase_test:
+            with mock.patch('chatbot.choice', self.mocked_random_choice):
+                bot_response = bot.randcase(test_case[KEY_INPUT])
+                
+                self.assertEqual(test_case[KEY_EXPECTED], bot_response)
+                
+                
+                
+        for test_case in self.app_login_requested_test:
+            with mock.patch('flask_socketio.SocketIO.emit', self.mocked_socketio_emit):
+                app.login_requested(test_case[KEY_INPUT])
+            
+if __name__ == '__main__':
+    unittest.main()
